2fc94ce472af7490a4ca5e514166a794779e0647
ivangordiychuk <ivangordiychuk@gmail.com>
2023-01-11 21:38:26 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+def update_dict(key, value, defaults={}):
+    defaults[key] = value
+    print(defaults)
+
+
+#update_dict(key='fruit', value='apple')
+#update_dict(key='vegetable', value='tomato', defaults={'tree': 'oak'})
+#update_dict(key='car', value='ferrari')
+
+import requests
+import asyncio
+import aiohttp
+import time
+
+start_time = time.time()
+async def get_page(session, category: str, page_id: int) -> str:
+    if page_id:
+        url = 'https://www.ozon.ru/brand/{0}/?page={1}'.format(category, page_id)
+    else:
+        url = 'https://www.ozon.ru/brand/{0}/'.format(category)
+
+    async with session.get(url) as response:
+        print('get url: {0}'.format(url))
+        response_text = await response.text()
+    return response_text
+
+
+async def load_data():
+    category_list = ['adidas-144082850', 'puma-87235756']
+    async with aiohttp.ClientSession() as session:
+        tasks = []
+        for category in category_list:
+            for page_id in range(50):
+                task = asyncio.create_task(get_page(session,category,page_id))
+                tasks.append(task)
+            # обрабатываем полученный текст, сохраняем в файл/базу
+        await asyncio.gather(*tasks)
+
+asyncio.run(load_data())
+
+end_time = time.time() - start_time
+print(f'\nExecution time: {end_time} seconds')
